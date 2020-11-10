88e8f1774e3e33d7c5f19623c7ae406753c68180
Allan Cruz <allan.cruz@unesp.br>
2020-11-10 14:43:35 -0300

    Adicionei os enunciados dos exercícios

--- Eloquent JS/02_program_structure/exercícios-02.js
+++ Eloquent JS/02_program_structure/exercícios-02.js
@@ -1,4 +1,17 @@
-//Exercício 1: Looping a triangle
+/*
+Exercício 1: Looping a triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
 //RESPOSTA:
 
 function ex_1() {
@@ -15,7 +28,14 @@ function ex_1() {
 
 
 
-//Exercício 2: FizzBuzz 
+/*
+Exercício 2: FizzBuzz
+
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+*/
+
 //RESPOSTA:
 
 function ex_2() {
@@ -43,7 +63,25 @@ function ex_2() {
 
 
 
-//Exercício 3: Chessboard
+/* 
+Exercício 3: Chessboard
+
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+*/
+
 //RESPOSTA:
 
 /*
@@ -64,7 +102,8 @@ function ex_3() {
     }
 }
 
-FAIL
+FAIL... essa forma não resolve a segunda parte do ex pq não é adaptavél pra qualquer tamanho de tabuleiro. Só aumentaria o numero de linhas. 9x8; 10x8; 11x8 ... etc.
+
 */
 
 
