e9c95e294fbfb57180a3627cd7242fe5abcb59db
Allan Cruz <allan.cruz@unesp.br>
2020-11-13 21:27:35 -0300

    Exercícios 2 e 3

--- Eloquent JS/03_functions/exercícios-03.js
+++ Eloquent JS/03_functions/exercícios-03.js
@@ -14,19 +14,19 @@ function min(number1, number2) {
         return number2;
     }
 } 
+//console.log(min(0,-10));
 
-console.log(min(0,-10));
 
 /* 
 Exercício 2: Recursion
 
-number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
+We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to see whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
 
-Zero is even.
+-Zero is even.
 
-One is odd.
+-One is odd.
 
-For any other number N, its evenness is the same as N - 2.
+-For any other number N, its evenness is the same as N - 2.
 
 Define a recursive function isEven corresponding to this description. The function should accept a single parameter (a positive, whole number) and return a Boolean.
 
@@ -35,3 +35,47 @@ Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to f
 
 //RESPOSTA:
 
+function isEven(number) {
+    if (number === 0) {
+        return true;
+    }
+    else if (number === 1) {
+        return false;
+    }
+    else if (number < 0) {
+        return isEven(-number);
+    }
+    else {
+        return isEven(number - 2);
+    }
+}
+//console.log(isEven(-1));
+
+
+/*
+Exercício 3: Bean counting
+
+You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+*/
+
+//RESPOSTA:
+
+function countChar(text, ch) {
+
+    let counter = 0;
+    
+    for (let letter_position = 0; letter_position < text.length; letter_position += 1) {
+        if (text[letter_position] === ch) {
+            counter += 1;
+        }
+    }
+    return counter;
+}
+
+function countBs(text) {
+    return countChar (text, "B");
+}
